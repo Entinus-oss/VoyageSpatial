animation bugging but wrong units

planetes.py
@@ -4,28 +4,66 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from functools import partial
 
+G = 6.674*1e-11
 
 class Planet():
 
     def __init__(self, m, coord_init, a=0, perihelie=0, e=0): #a : demi-grand axe, e: excentricité, vitesse en ration par rapport à la vitesse de la Terre
         self.m = m
-        #self.radius = radius
         self.coord = np.array(coord_init)
-        self.a = a
+        self.axe = a
         self.perihelie = perihelie
-        #self.aphelie = aphelie
         self.e = e
-        #self.v = v
-        self.u = -self.a+ self.perihelie
-        self.b = self.a*np.sqrt(1-self.e**2)
+        self.orbit = []
 
-    def actualisation_position(self,t, scatter):
-        self.coord = [self.u+self.a*np.cos(t), self.b*np.sin(t)]
-        scatter.set_offsets(self.coord)
+        self.u = -self.axe + self.perihelie
+        self.b = self.axe * np.sqrt(1 - self.e**2)
+        self.period = 2 * np.pi * np.sqrt(self.axe**3 / self.m / G)
+        self.omega = 2 * np.pi / self.period
+
+    # def planetVelocity(self, time): # time un array des temps 
+    #     vitesse = np.ones(2)
+        
+    #     self.coord = [self.u + self.axe * np.cos(time), self.b * np.sin(time)]
+    #     radius = np.linalg.norm(self.coord)
+    #     velocityScalar = np.sqrt(G * self.m * ((2 * self.axe - radius)/(self.axe * radius)))
+
+    #     vitesse[0] = velocityScalar * -self.axe * np.sin(time)
+    #     vitesse[1] = velocityScalar * np.cos(time)
+    #     vitesse = vitesse/np.linalg.norm(vitesse)
+
+    #     return vitesse
+
+    # def actualisation_position0(self, time, scatter):
+    #     # Calcule les coordonnées grâce à la vitesse 
+         
+    #     actualisationVitesse = self.planetVelocity(time)
+
+    #     self.coord = [self.coord[0] + actualisationVitesse[0], self.coord[1] + actualisationVitesse[1]]
+    #     scatter.set_offsets(self.coord)
+
+    def actualisation_position(self, time, T, N):
+        # Calcule les coordonnées grâce à la vitesse 
+        #self.perimeter = np.pi*(3*(self.axe+self.b) - np.sqrt(3*self.axe + self.b)*(self.axe * 3*self.b))
+        time = np.linspace(0, T, N)
+        for i in range(time.size):
+            print("time", time[i])
+            self.coord = np.array([self.u + self.axe * np.cos(self.omega * time[i]), self.b * np.sin(self.omega * time[i])])
+            self.orbit.append(self.coord)
+            print("coord", self.coord)
+
+
+    def animate(self, frame, scatter):  
+        # print(frame)
+        # print(len(self.orbit))
+        coord = self.orbit[frame]
+        #print("coord", coord)
+        scatter.set_offsets(coord)
+        #return scatter
 
 
 M = 5.972e24 
-mass = {"mercure" : 3.285e23,
+mass = {"mercure" : 3.285e23, # kg 
         "venus" : 4.867e24,
         "terre" : 5.972e24,
         "mars" : 6.390e23,
@@ -33,15 +71,15 @@ mass = {"mercure" : 3.285e23,
         "saturne" : 5.683e26,
         "uranus" : 8.681e25,
         "neptune" : 1.024e26}
-a = {"mercure" : 57.909083e6,
-    "venus" : 108.210e6,
-    "terre" : 149.598e6,
-    "mars" : 227.956e6,
-    "jupiter" : 778.479e6,
-    "saturne" : 1432.041e6,
-    "uranus" : 2867.043e6,
-    "neptune" : 4514.953e6}
-e = {"mercure" : 0.2056,
+a = {"mercure" : 57.909083e9, # m
+    "venus" : 108.210e9,
+    "terre" : 149.598e9,
+    "mars" : 227.956e9,
+    "jupiter" : 778.479e9,
+    "saturne" : 1432.041e9,
+    "uranus" : 2867.043e9,
+    "neptune" : 4514.953e9}
+e = {"mercure" : 0.2056, # sans dimension 
         "venus" : 0.0068,
         "terre" : 0.0167,
         "mars" : 0.0935,
@@ -49,14 +87,14 @@ e = {"mercure" : 0.2056,
         "saturne" : 0.0520,
         "uranus" : 0.0469,
         "neptune" : 0.0097}
-perihelie = {"mercure" : 46e6,
-            "venus" : 107.480e6,
-            "terre" : 147.095e6,
-            "mars" : 206.650e6,
-            "jupiter" : 740.595e6,
-            "saturne" : 1357.554e6,
-            "uranus" : 2732.696e6,
-            "neptune" : 4471.050e6}
+perihelie = {"mercure" : 46e9, # km
+            "venus" : 107.480e9,
+            "terre" : 147.095e9,
+            "mars" : 206.650e9,
+            "jupiter" : 740.595e9,
+            "saturne" : 1357.554e9,
+            "uranus" : 2732.696e9,
+            "neptune" : 4471.050e9}
 coordInit = {"mercure" : [perihelie["mercure"],0],
             "venus" : [perihelie["venus"],0],
             "terre" : [perihelie["terre"],0],

voyageSpatial.py
@@ -5,12 +5,27 @@ G = 6.674*1e-11
 
 class Planet():
 
-    def __init__(self, m, coord, v=0, radius=0):
+    def __init__(self, m, coord_init, a=0, perihelie=0, e=0):
+        #a : demi-grand axe, e: excentricité, vitesse en ration par rapport à la vitesse de la Terre
         self.m = m
-        self.radius = np.array(radius)
-        self.coord = np.array(coord)
-        self.x = coord[0]
-        self.y = coord[1]
+        self.coord = coord_init
+        self.axe = a # demi-grand axe
+        self.excentricite = e
+        self.perihelie = perihelie
+        self.u = -self.a+ self.perihelie
+        self.b = self.a*np.sqrt(1-self.e**2)
+        #self.radius = np.norm.linalg(self.coord)
+        #self.vitesse = np.sqrt(G*m*((2*self.axe - self.radius)/(self.axe*self.radius)))
+
+    def planetVelocity(self, time): # time un array des temps 
+        vitesse = np.zeros(len(time))
+        for i in range(len(time)) :
+            self.coord = [self.u+self.a*np.cos(time[i]), self.b*np.sin(time[i])]
+            rayon = np.norm.linalg(self.coord)
+            vitesse[i] = np.sqrt(G*m*((2*self.axe - self.radius)/(self.axe*self.radius)))
+        return vitesse  
+
+
     
 class Sonde():
 